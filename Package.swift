d467f17f8033193c8d41a904b176a40b38c199cb
jenkins <jenkins@regula.by>
2024-08-21 12:07:26 +0300

    Release 6.2.783

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["FaceCoreBasic"]),
     ],
     targets: [
-        .binaryTarget(name: "FaceCoreBasic", url: "https://pods.regulaforensics.com/FaceCoreBasic/6.2.773/FaceCoreBasic-6.2.773.zip", checksum: "2de80a29c7bd84b12abf325bfcc68c7fb6d4a81e4b9019fe7919044d9694103c"),
+        .binaryTarget(name: "FaceCoreBasic", url: "https://pods.regulaforensics.com/FaceCoreBasic/6.2.783/FaceCoreBasic-6.2.783.zip", checksum: "2a7adacfd074cda7c654860ebf51533150aa3d698d834b1240ab82fbd76f4d1c"),
     ]
 )
