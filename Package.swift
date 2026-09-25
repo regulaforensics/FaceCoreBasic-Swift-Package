de8aeedf3060de91ba3776c6de60cac1caf571df
Jenkins <jenkins@regula.by>
2026-09-25 03:28:32 +0300

    Nightly 8.4.2703

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     products: [
         .library(
             name: "FaceCoreBasic",
-            targets: ["FaceCoreBasicStage"]),
+            targets: ["FaceCoreBasicNightly"]),
     ],
     targets: [
-        .binaryTarget(name: "FaceCoreBasicStage", url: "https://pods.regulaforensics.com/Stage/FaceCoreBasicStage/8.4.2701/FaceCoreBasicStage-8.4.2701.zip", checksum: "c01b75a982a6ad5ff72ac66755248b9a54d4e56159e359051d4be5fcf1ca5654"),
+        .binaryTarget(name: "FaceCoreBasicNightly", url: "https://pods.regulaforensics.com/Nightly/FaceCoreBasicNightly/8.4.2703/FaceCoreBasicNightly-8.4.2703.zip", checksum: "a593b2d0e9c6f99c84f2d5050f0659ff225154405c21acdaa4c5f3c6c18d967e"),
     ]
 )
